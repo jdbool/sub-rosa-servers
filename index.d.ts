1b68140e27b39aaa4a6a1608f85e23664d1ea501
Misha <mgibich@gmail.com>
2019-08-15 20:51:13 -0500

    Updated typescript file

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,19 @@
-declare interface ServerData {
-  address: string;
-  port: number;
-  latency: number;
-  buffer: Buffer;
-  name: string;
-  version: number;
-  build: string;
-  clientCompatability: number;
-  passworded: boolean;
-  identifier: number;
-  gameType: number;
-  players: number;
-  maxPlayers: number;
+declare module 'sub-rosa-servers' {
+  interface ServerData {
+    address: string;
+    port: number;
+    latency: number;
+    buffer: Buffer;
+    name: string;
+    version: number;
+    build: string;
+    clientCompatability: number;
+    passworded: boolean;
+    identifier: number;
+    gameType: number;
+    players: number;
+    maxPlayers: number;
+  }
+  function getServerList(): Promise<ServerData[]>;
+  export = getServerList
 }
-
-export declare function getServerList(): Promise<ServerData[]>;
